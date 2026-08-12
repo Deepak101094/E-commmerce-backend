3aa5be050c8cceee9a6b9c48850f854ffae9459d
Deepak Pandey <deepakpandey101094@gmail.com>
2026-08-12 18:14:59 +0530

    refactor: separate health routes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,29 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.config import settings
+from app.api.health.router import router as health_router
 
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("🚀 Application startup")
+
+    yield
+
+    print("🛑 Application shutdown")
 
 def create_app() -> FastAPI:
+
     app = FastAPI(
     title=settings.APP_NAME,
-    version=settings.APP_VERSION
+    version=settings.APP_VERSION,
+    lifespan=lifespan
   )
 
-    @app.get("/health")
-    async def health_check():
-        return {"status": "ok"}
+    app.include_router(health_router)  
 
     return app 
 
 app = create_app()
+
